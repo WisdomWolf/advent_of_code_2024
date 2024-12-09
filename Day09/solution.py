e1e0a50727c427969c0de4b29c3b8b95c46f5a50
WisdomWolf <wisdomwolf@gmail.com>
2024-12-09 16:27:10 -0500

    Adds Day09

---
+++ Day09/solution.py
@@ -0,0 +1,15 @@
+
+
+
+def parse_input(lines):
+    pass
+
+
+def solution(lines):
+    pass
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        lines = f.readlines()
+        print(solution(lines))
